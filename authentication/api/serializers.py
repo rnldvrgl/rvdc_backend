f5fcda174aa4e1cda1787a5aadb4109d6dd9fdf7
Ronald Vergel Dela Cruz <delacruz.ronaldvergel@gmail.com>
2025-06-23 18:52:17 +0800

    feat: setting up

---
+++ authentication/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+
+f
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if not user:
+            raise serializers.ValidationError("Invalid credentials.")
+        return user
---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ("admin", "Admin"),
+        ("manager", "Manager"),
+        ("technician", "Technician"),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    birthday = models.DateField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    contact_number = models.CharField(max_length=15, null=True, blank=True)
+    profile_image = models.ImageField(
+        upload_to="profile_images/", null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.username
