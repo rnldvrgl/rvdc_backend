639249f1852a0942dc0d09460d9ec139182f4a7d
Ronald Vergel Dela Cruz <delacruz.ronaldvergel@gmail.com>
2025-07-18 15:59:33 +0800

    chore: prepare for deployment

--- config/settings.py
+++ config/settings.py
@@ -158,6 +158,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "page_size_query_param": "limit",
 }
 
 SIMPLE_JWT = {
