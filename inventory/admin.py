23c50ee7d1ce71aa8a678cac85e8ed04c851be8f
Ronald Vergel Dela Cruz <delacruz.ronaldvergel@gmail.com>
2025-07-21 11:29:29 +0800

    chore: register stockroom stock and stock to admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from inventory.models import Item, Stall, Stock, StockTransfer, StockTransferItem
+from inventory.models import (
+    Item,
+    Stall,
+    Stock,
+    StockTransfer,
+    StockTransferItem,
+    StockRoomStock,
+)
 
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Stall)
-admin.site.register(Stock)
 admin.site.register(StockTransfer)
 
 
@@ -19,3 +25,21 @@ class StockTransferItemAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+
+@admin.register(StockRoomStock)
+class StockRoomStockAdmin(admin.ModelAdmin):
+    list_display = ("item", "quantity")
+    list_filter = ("item",)
+    search_fields = ("item__name",)
+    ordering = ("item__name",)
+    raw_id_fields = ("item",)
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("stall", "item", "quantity")
+    list_filter = ("stall", "item")
+    search_fields = ("stall__name", "item__name")
+    ordering = ("stall__name", "item__name")
+    raw_id_fields = ("stall", "item")
