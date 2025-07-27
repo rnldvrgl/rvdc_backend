e938bd3b39737892cfa46f6e37dc0f41d9eca3f5
Ronald Vergel Dela Cruz <delacruz.ronaldvergel@gmail.com>
2025-07-27 18:01:24 +0800

    chore: simple admin page for remittance

--- remittances/admin.py
+++ remittances/admin.py
@@ -1,45 +1,56 @@
-# from django.contrib import admin
-# from .models import RemittanceRecord, CashDenominationBreakdown
+from django.contrib import admin
+from .models import RemittanceRecord, CashDenominationBreakdown
 
 
-# @admin.register(RemittanceRecord)
-# class RemittanceRecordAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "stall",
-#         "created_at",
-#         "is_remitted",
-#         "remitted_amount",
-#         "total_collected",
-#         "expected_remittance",
-#         "balance",
-#     )
-#     list_filter = ("stall", "is_remitted", "coins_remitted")
-#     search_fields = ("stall__name", "remitted_by__username")
-#     readonly_fields = (
-#         "expected_remittance",
-#         "total_collected",
-#         "balance",
-#         "updated_at",
-#     )
-#     date_hierarchy = "created_at"
+@admin.register(RemittanceRecord)
+class RemittanceRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        "stall",
+        "created_at",
+        "total_sales_cash",
+        "total_expenses",
+        "expected_remittance",
+        "declared_amount",
+        "remitted_amount",
+        "is_remitted",
+    )
+    list_filter = ("stall", "is_remitted", "created_at")
+    search_fields = ("stall__name", "notes")
+    readonly_fields = (
+        "expected_remittance",
+        "total_collected",
+        "balance",
+        "created_at",
+        "updated_at",
+    )
+    ordering = ("-created_at",)
 
-#     def total_collected(self, obj):
-#         return obj.total_collected
+    def expected_remittance(self, obj):
+        return obj.expected_remittance
 
-#     def expected_remittance(self, obj):
-#         return obj.expected_remittance
+    def total_collected(self, obj):
+        return obj.total_collected
 
-#     def balance(self, obj):
-#         return obj.balance
+    def balance(self, obj):
+        return obj.balance
+
+    expected_remittance.short_description = "Expected"
+    total_collected.short_description = "Collected Total"
+    balance.short_description = "Balance"
 
 
-# @admin.register(CashDenominationBreakdown)
-# class CashDenominationBreakdownAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "remittance",
-#         "total_remitted_amount",
-#         "cod_amount",
-#         "total_cash_declared",
-#     )
-#     search_fields = ("remittance__stall__name",)
-#     readonly_fields = ("total_remitted_amount", "cod_amount", "total_cash_declared")
+@admin.register(CashDenominationBreakdown)
+class CashDenominationBreakdownAdmin(admin.ModelAdmin):
+    list_display = (
+        "remittance",
+        "total_remitted_amount",
+        "total_cash_declared",
+        "cod_amount",
+    )
+    readonly_fields = (
+        "total_remitted_amount",
+        "total_cash_declared",
+        "cod_amount",
+        "cod_breakdown",
+    )
+    search_fields = ("remittance__stall__name",)
