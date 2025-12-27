3f255cde515b5926cab8c5f22f870938c1e94978
Ronald Vergel Dela Cruz <delacruz.ronaldvergel@gmail.com>
2025-12-27 11:48:33 +0800

    adjust deployment

---
+++ inventory/migrations/0035_remove_seed_stalls.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+
+
+def unseed_stalls(apps, schema_editor):
+    Stall = apps.get_model("inventory", "Stall")
+    for name, location in [
+        ("Main", "Service"),
+        ("Sub", "Parts")
+    ]:
+        stall = Stall.objects.filter(name=name, location=location).first()
+        if stall:
+            stall.delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0034_stall_inventory_enabled_stall_is_system'),
+    ]
+
+    operations = [
+        migrations.RunPython(unseed_stalls),
+    ]
